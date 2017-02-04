b7af8a341cbaa6893ed6498773ae95ba984337d5
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2017-02-04 12:32:25 -0200

    Model Person

--- syspet/core/models.py
+++ syspet/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    '''
+        Person
+    '''
+    company_name = models.CharField('razão social', max_length=255)
+    fantasy_name = models.CharField('nome fantasia', max_length=255)
+    TYPE_J = "Pessoa Jurídica"
+    TYPE_F = "Pessoa Fisíca"
+    TYPE_PERSON = (
+        ("F", TYPE_F),
+        ("J", TYPE_J),
+    )
+    person_type = models.CharField('tipo pessoa', max_length=1, choices=TYPE_PERSON)
+    cpnj_cpf = models.CharField('CNPJ/CPF', max_length=20)
+    address = models.CharField('logradouro', max_length=255, blank=True)
+    number_address = models.CharField('N.', max_length=60, blank=True)
+    neighborhood = models.CharField('bairro', max_length=255, blank=True)
+    zip_code = models.CharField('CEP', max_length=15, blank=True)
+    city = models.CharField('cidade', max_length=255, blank=True)
+    state = models.CharField('uf', max_length=2, blank=True)
+    phone = models.CharField('telefone', max_length=20, blank=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.company_name
--- syspet/core/tests.py
+++ syspet/core/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from syspet.core.models import Person
+
+
+class PersonModelTest(TestCase):
+    def setUp(self):
+        self.company_name = 'Razão1'
+        self.person = Person.objects.create(
+            company_name=self.company_name,
+            fantasy_name='Fantasia1',
+            person_type=Person.TYPE_F,
+            cpnj_cpf='000.000.000-00'
+        )
+
+    def test_create(self):
+        self.assertTrue(Person.objects.exists())
+
+    def test_str(self):
+        self.assertEqual(self.company_name, str(self.person))
